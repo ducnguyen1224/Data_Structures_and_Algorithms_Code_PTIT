980ba0b6f4cb1b3f7f9955418e072855aa26bf3d
vanhhn <vietanhvmo@gmail.com>
2024-03-24 17:15:06 +0700

    first commit

--- CTDL103.cpp
+++ CTDL103.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[1005][1005];
+
+int a[1001][1001];
 int main()
-{ int t;
-  cin>>t;
-  while(t--){
-  memset(a,0,sizeof(a));
+{ 
   int n;
   cin>>n;
-  cin.ignore();
+  int x;
+  char c;
   for(int i=1;i<=n;i++){
-    string s;
-    getline(cin,s);
-    stringstream ss(s);
-    string token;
-    while(ss>>token){
-      a[i][stoll(token)]=1;
-      a[stoll(token)][i]=1;
-    }
+  	while(scanf("%d%c",&x,&c)!=EOF){
+  		a[i][x]=1;
+  		if(c=='\n'){
+  			break;
+		  }
+	  }
   }
   for(int i=1;i<=n;i++){
-    for(int j=1;j<=n;j++){
-      cout<<a[i][j]<<" ";
-    }
-    cout<<"\n";
-  }
+  	for(int j=1;j<=n;j++){
+  		cout<<a[i][j]<<" ";
+	  }
+	  cout<<"\n";
   }
  return 0;
 }
